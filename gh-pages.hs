6393875b2f95391df10bed0e3b052e04b2153b6b
dekokun <shintaro.kurachi@gmail.com>
2012-07-23 23:40:06 +0900

    ブログ的に変更

--- gh-pages.hs
+++ gh-pages.hs
@@ -1,17 +1,58 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
-import Control.Category ((>>>))
+
+import Prelude hiding (id)
+import Control.Category (id)
+import Control.Arrow ((>>>), (***), arr)
+import Data.Monoid (mempty, mconcat)
+
 import Hakyll
 
 main :: IO ()
-main = hakyllWith config $ do
-  match "templates/*" $ compile templateCompiler
-  match (list ["index.markdown"]) $ do
-    route $ setExtension "html"
-    compile $ pageCompiler
-      >>> applyTemplateCompiler "templates/default.hamlet"
-      >>> relativizeUrlsCompiler
+main = hakyll $ do
+    -- Compress CSS
+    match "css/*" $ do
+        route idRoute
+        compile compressCssCompiler
 
+    -- Render posts
+    match "posts/*" $ do
+        route $ setExtension ".html"
+        compile $ pageCompiler
+            >>> applyTemplateCompiler "templates/post.html"
+            >>> applyTemplateCompiler "templates/default.html"
+            >>> relativizeUrlsCompiler
+
+    -- Render posts list
+    match "posts.html" $ route idRoute
+    create "posts.html" $ constA mempty
+        >>> arr (setField "title" "All posts")
+        >>> requireAllA "posts/*" addPostList
+        >>> applyTemplateCompiler "templates/posts.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Index
+    match "index.html" $ route idRoute
+    create "index.html" $ constA mempty
+        >>> arr (setField "title" "机上日記")
+        >>> requireAllA "posts/*" (id *** arr (take 3 . reverse . sortByBaseName) >>> addPostList)
+        >>> applyTemplateCompiler "templates/index.html"
+        >>> applyTemplateCompiler "templates/default.html"
+        >>> relativizeUrlsCompiler
+
+    -- Read templates
+    match "templates/*" $ compile templateCompiler
+
+-- | Auxiliary compiler: generate a post list from a list of given posts, and
+-- add it to the current page under @$posts@
+--
+addPostList :: Compiler (Page String, [Page String]) (Page String)
+addPostList = setFieldA "posts" $
+    arr (reverse . sortByBaseName)
+        >>> require "templates/position.html" (\p t -> map (applyTemplate t) p)
+        >>> arr mconcat
+        >>> arr pageBody
 config :: HakyllConfiguration
 config = defaultHakyllConfiguration { deployCommand = deploy }
   where deploy = "cp -r _site/* . && runghc gh-pages.hs clean"
